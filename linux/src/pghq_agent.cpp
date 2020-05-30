9da061f3db22b301fecba848fc8b23052f42a006
PGHeadQuarters <66076447+pgheadquarter@users.noreply.github.com>
2020-05-30 11:42:03 +0100

    new adds

--- linux/src/pghq_agent.cpp
+++ linux/src/pghq_agent.cpp
@@ -8,6 +8,11 @@
 
 #include <iostream>
 #include "shared_src/cmdserver/StandardServer.h"
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/initializer.h>
+#include "shared_src/cmdserver/StandardServer.h"
 
 
 using namespace std;
@@ -16,7 +21,17 @@ int main(int argc, char **argv)
 {
 	cout << "PGHQ AGENT" << endl; // prints !!!Hello World!!!
 
-	start_standard_server( argc, argv);
+    //start_standard_server( argc, argv);
+
+    // Initialization and deinitialization.
+    log4cplus::Initializer initializer;
+
+    log4cplus::BasicConfigurator config;
+    config.configure();
+
+    log4cplus::Logger logger = log4cplus::Logger::getInstance(
+    LOG4CPLUS_TEXT("main"));
+    LOG4CPLUS_WARN(logger, LOG4CPLUS_TEXT("Hello, World!"));
 
 	return 0;
 }
